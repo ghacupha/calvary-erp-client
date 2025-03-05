558ba6defcb473fd9cf5235e0df448a0f4d5c0dc
Edwin Njeru <mailnjeru@gmail.com>
2025-03-05 13:11:51 +0300

    Minor cleanup, unused code

--- src/main/webapp/app/extensions/registration/application-user/list/application-user.component.ts
+++ src/main/webapp/app/extensions/registration/application-user/list/application-user.component.ts
@@ -16,25 +16,12 @@
 /// along with this program. If not, see <http://www.gnu.org/licenses/>.
 ///
 
-import { Component, NgZone, OnInit, inject } from '@angular/core';
-import { HttpHeaders } from '@angular/common/http';
-import { ActivatedRoute, Data, ParamMap, Router, RouterModule } from '@angular/router';
-import { Observable, Subscription, combineLatest, filter, tap } from 'rxjs';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-import SharedModule from 'app/shared/shared.module';
-import { SortByDirective, SortDirective, SortService, type SortState, sortStateSignal } from 'app/shared/sort';
-import { DurationPipe, FormatMediumDatePipe, FormatMediumDatetimePipe } from 'app/shared/date';
-import { ItemCountComponent } from 'app/shared/pagination';
 import { FormsModule } from '@angular/forms';
-import { ITEMS_PER_PAGE, PAGE_HEADER, TOTAL_COUNT_RESPONSE_HEADER } from 'app/config/pagination.constants';
-import { DEFAULT_SORT_DATA, ITEM_DELETED_EVENT, SORT } from 'app/config/navigation.constants';
-import { FilterComponent, FilterOptions, IFilterOption, IFilterOptions } from 'app/shared/filter';
-import { IApplicationUser } from '../application-user.model';
+import SharedModule from 'app/shared/shared.module';
 
-import { ApplicationUserService } from '../../../../entities/application-user/service/application-user.service';
-import { EntityArrayResponseType } from '../service/application-user.service';
-import { ApplicationUserDeleteDialogComponent } from '../../../../entities/application-user/delete/application-user-delete-dialog.component';
 import { ApplicationUserComponent } from '../../../../entities/application-user/list/application-user.component';
 
 @Component({
@@ -42,18 +29,6 @@ import { ApplicationUserComponent } from '../../../../entities/application-user/
   selector: 'jhi-erp-application-user',
   styleUrls: ['./application-user.styles.scss'],
   templateUrl: './application-user.component.html',
-  imports: [
-    RouterModule,
-    FormsModule,
-    SharedModule,
-    SortDirective,
-    SortByDirective,
-    DurationPipe,
-    FormatMediumDatetimePipe,
-    FormatMediumDatePipe,
-    FilterComponent,
-    ItemCountComponent,
-    ApplicationUserComponent,
-  ],
+  imports: [RouterModule, FormsModule, SharedModule, ApplicationUserComponent],
 })
 export class ERPApplicationUserComponent extends ApplicationUserComponent implements OnInit {}
